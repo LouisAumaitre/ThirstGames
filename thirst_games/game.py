fa13d2c33e2a47c5d9d4b57f7a1c6786ace59a3a
Louis Aumaitre <louis.aumaitre@tiime.fr>
2018-07-25 15:20:23 +0200

    add Map

--- thirst_games/game.py
+++ thirst_games/game.py
@@ -1,6 +1,19 @@
+from thirst_games.map import Map
+
+
 class Game:
     def __init__(self, players: list):
         self.players = players
+        self.alive_players = players
+        self.map = Map()
+        for p in self.players:
+            self.map.add_player(p)
 
     def run(self):
+        day = 1
+        while len(self.alive_players) > 1 or day < 10:
+            print(f'== DAY {day} ==')
+            self.day()
+
+    def day(self):
         pass
---
+++ thirst_games/map.py
@@ -0,0 +1,22 @@
+from random import random
+
+START_AREA = 'the cornucopea'
+
+
+class Map:
+    def __init__(self, size=4):
+        possible_parts = [
+            'the green forest', 'the red forest', 'the white forest', 'the rocks', 'the jungle', 'the river', 'the hill'
+        ]
+        self.areas = {area_name: [] for area_name in possible_parts.sort(key=lambda x: random())[0:size-1]}
+        self.areas[START_AREA] = []
+
+    def add_player(self, player):
+        player.current_area = START_AREA
+        self.areas[START_AREA].append(player)
+
+    def move_player(self, player, new_area):
+        self.areas[player.current_area].remove(player)
+        self.areas[new_area].append(player)
+        player.current_area = new_area
+        print(f'{player.name} moves to {new_area}')
